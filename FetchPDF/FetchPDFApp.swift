f02c3fb9e9eaa81885a81ebc244005469528ffac
Stef Kors <stef.kors@gmail.com>
2025-04-28 20:01:31 +0200

    Initial Commit

---
+++ FetchPDF/FetchPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchPDFApp.swift
+//  FetchPDF
+//
+//  Created by Stef Kors on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FetchPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
